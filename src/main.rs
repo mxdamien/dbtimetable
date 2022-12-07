6936717fa3f681b917daefecadc60ca77099d6fa
David Langhals <david.langhals@gmx.net>
2022-12-07 16:27:07 +0100

    Make presenter type configurable

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ mod dbapiclient;
 mod timetable;
 mod timetablepresenter;
 mod timetablepresenterconsole;
+mod timetablepresenterjson;
 mod xmlparser;
 
 use app::App;
 use config::Config;
 use dbapiclient::DbApiClient;
 use timetablepresenterconsole::TimetablePresenterConsole;
+use timetablepresenterjson::TimetablePresenterJson;
 use xmlparser::XmlParser;
 
 extern crate confy;
@@ -24,12 +26,21 @@ fn load_config() -> Config {
     }
 }
 
+fn create_presenter(
+    presentertype: &config::PresenterType,
+) -> Box<dyn timetablepresenter::TimetablePresenter> {
+    match presentertype {
+        config::PresenterType::Console => Box::new(TimetablePresenterConsole::new()),
+        config::PresenterType::Json => Box::new(TimetablePresenterJson::new()),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = load_config();
     let apiclient = DbApiClient::new(config.clone());
     let xmlparser = XmlParser::new();
-    let presenter = Box::new(TimetablePresenterConsole::new());
+    let presenter = create_presenter(&config.presenter);
     let app = App::new(apiclient, xmlparser, config.clone(), presenter);
     app.run().await;
 
